2d71e51fddf9c311f0f3acd039b8a2a5f513c46e
LaPreeH <157357331+LaPreeH@users.noreply.github.com>
2026-09-09 13:46:13 -0500

    Add Bubble Sort implementation in BubbleSort.cpp
    Implemented a simple Bubble Sort algorithm to sort an array.

---
+++ Bubble Sort Prac/BubbleSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int arr[10] = {1,6,3,3,7,45,243,55,32,54};
+    int size = 10;
+
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size - 1; j++){ 
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
